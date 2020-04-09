aba2af43ce093c1844cf8e959ec21a508a333cb7
James <mrjames8124@gmail.com>
2020-04-08 17:53:00 -0700

    finished structure issues with importing mods

---
+++ client/components/app.jsx
@@ -0,0 +1,14 @@
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
