46d36dba025af82fffc49e435a8fb999fc3ec082
James <mrjames8124@gmail.com>
2020-04-09 17:27:34 -0700

    buttons added with boostrap

--- client/components/filters.jsx
+++ client/components/filters.jsx
@@ -5,12 +5,12 @@ export default function Filters(props) {
   return (
     <div className="d-flex">
       <div>
-        <button className="btn-primary">Type</button>
-        <button className="btn-primary ml-3">Location</button>
-        <button className="btn-primary ml-3">Budget</button>
+        <button className="btn-outline-primary">Type</button>
+        <button className="btn-outline-primary ml-3">Location</button>
+        <button className="btn-outline-primary ml-3">Budget</button>
       </div>
-      <div className="btn-primary offset-5">
-        <button>Spin</button>
+      <div className=" offset-5">
+        <button className="btn-primary">Spin</button>
       </div>
     </div>
   );
