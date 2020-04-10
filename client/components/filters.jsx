4aadf579ea018f0ee5419d285923a8ba58e62be7
James <mrjames8124@gmail.com>
2020-04-09 17:21:41 -0700

    buttons created

--- client/components/filters.jsx
+++ client/components/filters.jsx
@@ -4,6 +4,14 @@ export default function Filters(props) {
 
   return (
     <div>
+      <div>
+        <button>Type</button>
+        <button>Location</button>
+        <button>Budget</button>
+      </div>
+      <div>
+        <button>Spin</button>
+      </div>
     </div>
   );
 }
