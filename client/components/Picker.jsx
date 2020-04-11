1d2d82fb7ce20151b5c788a0f718c17ba9d66d02
James <mrjames8124@gmail.com>
2020-04-11 16:25:27 -0700

    testing with hard coded foods

--- client/components/Picker.jsx
+++ client/components/Picker.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 export default function Picker(props) {
 
   return (
-    <div>
+    <div className="spinner-content">
+      <div>Pizza</div>
+      <div>Burgers</div>
+      <div>Fries</div>
+      <div>Soup</div>
+      <div>Chicken</div>
+      <div>Boba</div>
+      <div>Shakes</div>
+      <div>Pho</div>
+      <div>Rice</div>
+      <div>Bread</div>
     </div>
   );
 }
