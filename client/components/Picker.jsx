a90341954e8f23f7550b4da72e5cb9963f476029
James <mrjames8124@gmail.com>
2020-04-11 16:53:22 -0700

    adjusting spin posiiton

--- client/components/Picker.jsx
+++ client/components/Picker.jsx
@@ -4,16 +4,18 @@ export default function Picker(props) {
 
   return (
     <div className="spinner-content">
-      <div className="m-4">Pizza</div>
-      <div className="m-4">Burgers</div>
-      <div className="m-4">Fries</div>
-      <div className="m-4">Soup</div>
-      <div className="m-4">Chicken</div>
-      <div className="m-4">Boba</div>
-      <div className="m-4">Shakes</div>
-      <div className="m-4">Pho</div>
-      <div className="m-4">Rice</div>
-      <div className="m-4">Bread</div>
+      <div className="spinner">
+        <div className="m-4">Pizza</div>
+        <div className="m-4">Burgers</div>
+        <div className="m-4">Fries</div>
+        <div className="m-4">Soup</div>
+        <div className="m-4">Chicken</div>
+        <div className="m-4">Boba</div>
+        <div className="m-4">Shakes</div>
+        <div className="m-4">Pho</div>
+        <div className="m-4">Rice</div>
+        <div className="m-4">Bread</div>
+      </div>
     </div>
   );
 }
