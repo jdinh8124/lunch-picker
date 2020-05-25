9675dd81d184e863ac2fad1ec8010523effde73f
James <mrjames8124@gmail.com>
2020-05-24 19:55:33 -0700

    working through wheel spin issues

--- client/components/Picker.jsx
+++ client/components/Picker.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 export default function Picker(props) {
 
   return (
-    <div className="spinner-content">
-      <div className="spinner">
-        <div className="m-4">Pizza</div>
-        <div className="m-4">Burgers</div>
-        <div className="m-4">Fries</div>
-        <div className="m-4">Soup</div>
-        <div className="m-4">Chicken</div>
-        <div className="m-4">Boba</div>
-        <div className="m-4">Shakes</div>
-        <div className="m-4">Pho</div>
-        <div className="m-4">Rice</div>
-        <div className="m-4">Bread</div>
+    <div className="scene">
+      <div className="spinner-content">
+        <div className="spinner">
+          <div className="m-4 carousel__cell">Pizza</div>
+          <div className="m-4 carousel__cell">Burgers</div>
+          <div className="m-4 carousel__cell">Fries</div>
+          <div className="m-4 carousel__cell">Soup</div>
+          <div className="m-4 carousel__cell">Chicken</div>
+          <div className="m-4 carousel__cell">Boba</div>
+          <div className="m-4 carousel__cell">Shakes</div>
+          <div className="m-4 carousel__cell">Pho</div>
+          <div className="m-4 carousel__cell">Rice</div>
+          <div className="m-4 carousel__cell">Bread</div>
+        </div>
       </div>
     </div>
   );
